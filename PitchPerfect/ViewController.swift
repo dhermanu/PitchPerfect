bbed448eaceb09ab4c27afa8f48e53bbb77f84ed
Dean Hermanudin <dean@life360.com>
2017-07-31 17:33:38 -0700

    Completed Lesson 1: Added mock buttons and outltes

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordinButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // stuff
+        super.viewWillAppear(animated)
+    }
 
 
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
+    
+    @IBAction func stopRecordingAudio(_ sender: Any) {
+        print("stop recording button was pressed")
+    }
 }
 
